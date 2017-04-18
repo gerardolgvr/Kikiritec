8bca32a104668d2f2418fcbc0ad2f238c0a2b052
Augusto Neftali Ruiz Cauich <augusto.ruizcauich@gmail.com>
2017-04-17 19:00:10 -0500

    agregue el actovity de registro de clases

---
+++ app/src/main/java/mx/com/collegedays/collegedays/Activities/RegistroClase.java
@@ -0,0 +1,15 @@
+package mx.com.collegedays.collegedays.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.com.collegedays.collegedays.R;
+
+public class RegistroClase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_clase);
+    }
+}
